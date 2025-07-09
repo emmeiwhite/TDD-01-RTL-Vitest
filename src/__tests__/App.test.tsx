3964e667bb3186f2753683305f3e0fcd26971a82
emmeiwhite <emmeiwhite@gmail.com>
2025-07-09 23:27:21 +0530

    TDD-2 Tests without assertion will pass

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -12,8 +12,18 @@ test('should render heading with correct text', () => {
   screen.debug()
 
   // Find heading by its text content
-  const heading = screen.getByText('React Testing Library')
+  // const heading = screen.getByText('React Testing Library')
 
   // Verify heading exists in document
-  expect(heading).toBeInTheDocument()
+  expect(screen.getByText('React Testing Library')).toBeInTheDocument()
+})
+
+/** No Assertion --- tests will always pass */
+test('empty test', () => {
+  // Empty test will always pass, because we have no assertion to validate failure
+})
+
+test('this test will also pass', () => {
+  const sum = 2 + 3
+  console.log(sum)
 })
