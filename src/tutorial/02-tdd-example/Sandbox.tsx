9f7293c577ba821ca3b662b6ed92d60bbca7f1e5
emmeiwhite <emmeiwhite@gmail.com>
2025-07-10 08:30:51 +0530

    TDD-13 TDD in action --- wrote mininul code to pass the test

--- src/tutorial/02-tdd-example/Sandbox.tsx
+++ src/tutorial/02-tdd-example/Sandbox.tsx
@@ -1,4 +1,8 @@
 const Sandbox = () => {
-  return <div>Sandbox</div>;
-};
-export default Sandbox;
+  return (
+    <div>
+      <h4>Testing React Component</h4>
+    </div>
+  )
+}
+export default Sandbox
