8cc4b074e7ea5d2bb6345a2090e8632fc35a90b7
emmeiwhite <emmeiwhite@gmail.com>
2025-07-10 12:50:38 +0530

    TDD-21 tumbled upon .toHaveTextContext()

--- src/tutorial/04-user-interactions/Sandbox.test.tsx
+++ src/tutorial/04-user-interactions/Sandbox.test.tsx
@@ -2,6 +2,14 @@ import { screen, render, logRoles } from '@testing-library/react'
 import Sandbox from './Sandbox'
 
 describe('04-user-interactions with userEvent', () => {
+  test('count is initially 0', () => {
+    render(<Sandbox />)
+
+    const heading1 = screen.getByRole('heading', { name: '0', level: 1 })
+
+    expect(heading1).toHaveTextContent('0')
+  })
+
   test('counter button are present', () => {
     const { container } = render(<Sandbox />)
 
