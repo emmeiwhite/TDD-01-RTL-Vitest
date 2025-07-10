c5665ee116d945cacbdccd6e2a46d79c17133e8d
emmeiwhite <emmeiwhite@gmail.com>
2025-07-10 11:20:18 +0530

    TDD-16 Roles covered: heading, img, button (Synchronous - No Condtion)

--- src/tutorial/03-search-by-role/Sandbox.test.tsx
+++ src/tutorial/03-search-by-role/Sandbox.test.tsx
@@ -1,10 +1,11 @@
-import { screen, render } from '@testing-library/react'
+import { screen, render, logRoles } from '@testing-library/react'
 import Sandbox from './Sandbox'
 
 describe('03. Search By Role', () => {
   test('renders nav and navigation links', () => {
-    render(<Sandbox />)
+    const { container } = render(<Sandbox />)
 
+    logRoles(container)
     // 1. test navigation
     const navigation = screen.getByRole('navigation')
     expect(navigation).toBeInTheDocument()
@@ -13,5 +14,29 @@ describe('03. Search By Role', () => {
 
     expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument()
     expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument()
+  })
+
+  test('renders headings with correct hierarchy', () => {
+    render(<Sandbox />)
+
+    //   3. test heading which has role heading
+    expect(screen.getByRole('heading', { name: 'Main Heading', level: 1 })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: 'SubHeading', level: 2 })).toBeInTheDocument()
+  })
+
+  test('renders image with alt text', () => {
+    render(<Sandbox />)
+
+    //   4. img with alt (alt in jsDOM env is treated as name property)
+    expect(screen.getByRole('img', { name: 'Example' })).toBeInTheDocument()
+  })
+
+  test('render initial buttons', () => {
+    render(<Sandbox />)
+
+    //  5. buttons
+    expect(screen.getByRole('button', { name: 'Click me' })).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: 'Submit' })).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: 'Cancel' })).toBeInTheDocument()
   })
 })
